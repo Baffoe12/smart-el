22628496e01f0d36ebeefa2f53dacf3700c637e1
Your Name <you@example.com>
2025-08-13 07:00:13 +0000

    hho

--- models/Appliance.js
+++ models/Appliance.js
@@ -1,18 +1,37 @@
-module.exports = (sequelize) => {
+// models/Appliance.js
+module.exports = (sequelize, DataTypes) => {
   const Appliance = sequelize.define('Appliance', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true
     },
-    name: DataTypes.STRING,
-    relay: DataTypes.INTEGER,
-    type: DataTypes.STRING,
-    status: DataTypes.STRING,
-    manuallyAdded: DataTypes.BOOLEAN
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    relay: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.STRING
+    },
+    status: {
+      type: DataTypes.STRING
+    },
+    manuallyAdded: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    tableName: 'Appliances'
   });
 
   Appliance.associate = function(models) {
-    Appliance.hasMany(models.SensorData, { foreignKey: 'applianceId' });
+    Appliance.hasMany(models.SensorData, {
+      foreignKey: 'applianceId',
+      as: 'sensorData'
+    });
   };
 
   return Appliance;
