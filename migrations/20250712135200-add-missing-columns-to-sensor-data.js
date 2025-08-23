e7444c7106afa99cc2d7982cb19f3606478b7cc6
Your Name <you@example.com>
2025-08-23 00:29:30 +0000

    vim

--- migrations/20250712135200-add-missing-columns-to-sensor-data.js
+++ migrations/20250712135200-add-missing-columns-to-sensor-data.js
@@ -1,15 +1,14 @@
-//life 
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('SensorData', 'energy_kwh', {
+    await queryInterface.addColumn('sensor_data', 'energy_kwh', {
       type: Sequelize.FLOAT,
       allowNull: false,
       defaultValue: 0.0
     });
-    
-    await queryInterface.addColumn('SensorData', 'cost_ghs', {
+
+    await queryInterface.addColumn('sensor_data', 'cost_ghs', {
       type: Sequelize.FLOAT,
       allowNull: false,
       defaultValue: 0.0
@@ -17,7 +16,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('SensorData', 'energy_kwh');
-    await queryInterface.removeColumn('SensorData', 'cost_ghs');
+    await queryInterface.removeColumn('sensor_data', 'energy_kwh');
+    await queryInterface.removeColumn('sensor_data', 'cost_ghs');
   }
 };
