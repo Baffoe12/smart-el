28ef783bc02c22fa9c442de986a04a7e129509f5
Your Name <you@example.com>
2025-08-23 00:03:06 +0000

    change

--- migrations/20250712134000-add-name-to-users.js
+++ migrations/20250712134000-add-name-to-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Users', 'name', {
+   await queryInterface.addColumn('users', 'name', {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: 'User' // Default value for existing records
--- models/User.js
+++ models/User.js
@@ -1,16 +1,29 @@
-// models/User.js
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: { type: DataTypes.STRING },
-    email: { type: DataTypes.STRING, unique: true },
-    passwordHash: { type: DataTypes.STRING }
+    name: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     underscored: true,
-    timestamps: true
+    timestamps: true,
+    tableName: 'users'  // Explicitly set
   });
 
   User.associate = function(models) {
-    // Add associations if needed
+    // Add associations later if needed
+    // e.g., User.hasMany(models.Appliance)
   };
 
   return User;
