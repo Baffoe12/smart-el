3b4569ded62023bc0d9fa682ae7e8a653438fde8
Your Name <you@example.com>
2025-08-23 01:00:26 +0000

    Add energy_kwh and cost_ghs safely

--- migrations/20250712135200-add-missing-columns-to-sensor-data.js
+++ migrations/20250712135200-add-missing-columns-to-sensor-data.js
@@ -2,21 +2,34 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('sensor_data', 'energy_kwh', {
-      type: Sequelize.FLOAT,
-      allowNull: false,
-      defaultValue: 0.0
-    });
+    const tableInfo = await queryInterface.describeTable('sensor_data');
 
-    await queryInterface.addColumn('sensor_data', 'cost_ghs', {
-      type: Sequelize.FLOAT,
-      allowNull: false,
-      defaultValue: 0.0
-    });
+    if (!tableInfo.energy_kwh) {
+      await queryInterface.addColumn('sensor_data', 'energy_kwh', {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0.0
+      });
+    }
+
+    if (!tableInfo.cost_ghs) {
+      await queryInterface.addColumn('sensor_data', 'cost_ghs', {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0.0
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('sensor_data', 'energy_kwh');
-    await queryInterface.removeColumn('sensor_data', 'cost_ghs');
+    const tableInfo = await queryInterface.describeTable('sensor_data');
+
+    if (tableInfo.cost_ghs) {
+      await queryInterface.removeColumn('sensor_data', 'cost_ghs');
+    }
+
+    if (tableInfo.energy_kwh) {
+      await queryInterface.removeColumn('sensor_data', 'energy_kwh');
+    }
   }
 };
